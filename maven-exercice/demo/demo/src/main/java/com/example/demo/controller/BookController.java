245c4ba39c850893434ba6c6924f59a0194476a8
Guilherme <l0rd.v4dher@gmail.com>
2025-08-19 21:52:05 -0300

    initial commit

---
+++ maven-exercice/demo/demo/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getProduct(@PathVariable("id") Long id) {
+        if(bookRepository.existsById(id)) 
+            return bookRepository.findById(id).get();
+    
+        return null;
+    }
+}
---
+++ maven-exercice/demo/demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_BOOK (id, title, author, publish_year) VALUES (1, 'Dom Casmurro', 'Machado de Assis', '1899-01-01');
+INSERT INTO TBL_BOOK (id, title, author, publish_year) VALUES (2, 'O Alquimista', 'Paulo Coelho', '1988-05-01');
+INSERT INTO TBL_BOOK (id, title, author, publish_year) VALUES (3, 'Cem Anos de Solidão', 'Gabriel García Márquez', '1967-06-05');
